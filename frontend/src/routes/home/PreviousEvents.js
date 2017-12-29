e40040b5b00b42c9c36c05bb88d1a1dd4f3b6d33
Richard Van Camp <richard.vancamp@gmail.com>
2017-12-29 21:25:29 +0200

    Move events data to json for easier updating

--- frontend/src/routes/home/PreviousEvents.js
+++ frontend/src/routes/home/PreviousEvents.js
@@ -1,9 +1,9 @@
-import { h, Component } from 'preact'
-import styled, { css } from 'styled-components'
+import { h } from 'preact'
+import styled from 'styled-components'
 import YouTube from 'react-youtube'
 
+import events from '../../../../events.json'
 import { theme } from '../../style/theme'
-
 import { Grid, Cell } from '../../components/layout'
 
 const Wrapper = styled.div`
@@ -34,103 +34,40 @@ const MeetupDetails = styled.div`
 
 const YOUTUBE_OPTS = { height: '180', width: '320' }
 
-const EventPanel = ({
-  youtubeId,
-  speakers,
-  title,
-  titleLink,
-  links,
-  details
-}) => {
+const EventPanel = ({ youtubeId, title, titleLink, details, startsFrom }) => {
   const speakersAndMeetup = details.split('-')
+  const start = startsFrom ? `#t=${startsFrom}` : ''
   return (
     <Cell>
       {title && !titleLink && <EventPanelTitle>{title}</EventPanelTitle>}
       {title &&
         titleLink && (
-          <a href={titleLink}>
+          <a href={`${titleLink}${start}`}>
             <EventPanelTitle>{title}</EventPanelTitle>
           </a>
         )}
       <MeetupDetails>{speakersAndMeetup[0]}</MeetupDetails>
       <MeetupDetails>{speakersAndMeetup[1]}</MeetupDetails>
       {youtubeId && <YouTube videoId={youtubeId} opts={YOUTUBE_OPTS} />}
-      {links &&
-        links.length &&
-        links.map(l => (
-          <a href={l.href} key={href}>
-            {l.title}
-          </a>
-        ))}
     </Cell>
   )
 }
 
-const PreviousEvents = () => {
-  return (
-    <Wrapper>
-      <EventTitle>Previous Events</EventTitle>
-      <Grid columns="repeat(auto-fit,minmax(330px,1fr))">
+const PreviousEvents = () => (
+  <Wrapper>
+    <EventTitle>Previous Events</EventTitle>
+    <Grid columns="repeat(auto-fit,minmax(330px,1fr))">
+      {events.talks.map(t => (
         <EventPanel
-          youtubeId="pPxpUJyg1ss"
-          title="The Micro-Christmas Hackathon"
-          titleLink="https://battle3.site/"
-          details="Leo Melin, Richard Van Camp, Teemu Erkkola - Vol. 4, Wapice, 2017-08-17"
+          title={t.title}
+          titleLink={t.titleLink}
+          details={t.details}
+          youtubeId={t.youtubeId}
+          startsFrom={t.startsFrom}
         />
-        <EventPanel
-          youtubeId="1n4BbAfbt5w"
-          title="WB&S Coders' quiz"
-          details="Richard Van Camp, Mikko Matilainen & Leo Melin - Vol. 4, Wapice, 2017-08-17"
-        />
-        <EventPanel
-          youtubeId="i78KpYX9ooc"
-          title="Why some API's suck"
-          details="Viljami Kuosmanen - Vol. 4, Wapice, 2017-08-17"
-        />
-        <EventPanel
-          youtubeId="4YKilvpDOFI"
-          title="Uh...Yeah...Let's talk about Polymer, then..."
-          details="Fabiano Brito & André Valgrande - Vol. 4, Wapice, 2017-08-17"
-        />
-        <EventPanel
-          youtubeId="QOz3mdlSr5k"
-          title="One microservice success story"
-          details="Jaakko Takaluoma - Vol. 4, Wapice, 2017-08-17"
-        />
-        <EventPanel
-          youtubeId="JXKIaZRJAS0"
-          title="The Web Assembly Challenge"
-          details="Teemu Erkkola, Jaakko Huuso, & Rick VC - Vol. 3, Vincit, 2017-01-11"
-        />
-        <EventPanel
-          youtubeId="JXKIaZRJAS0"
-          title="Skip the backend completely using Nginx + embedded Luo"
-          details="Onni Hakala - Vol. 3, Vincit, 2017-01-11"
-        />
-        <EventPanel
-          youtubeId="dlJLDoJ0OqQ"
-          title="Web Audio"
-          details="Tero Parviainen - Vol 1., Gofore, 2016-08-18"
-        />
-
-        <EventPanel
-          youtubeId="iu0w96wKJEk"
-          title="CSS Modules"
-          details="Riku Ruovila - Vol 1., Gofore, 2016-08-18"
-        />
-        <EventPanel
-          youtubeId="nEn5k_HXeNc"
-          title="React API design: Case Rectabular"
-          details="Juho Vepsäläinen - Vol 1., Gofore, 2016-08-18"
-        />
-        <EventPanel
-          youtubeId="R_Ac8l4BifI"
-          title="A sneak peek at WebAssembly"
-          details="Leo Melin - Vol 1., Gofore, 2016-08-18"
-        />
-      </Grid>
-    </Wrapper>
-  )
-}
+      ))}
+    </Grid>
+  </Wrapper>
+)
 
 export default PreviousEvents
