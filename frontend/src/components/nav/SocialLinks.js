599f6c32283cee72f0276decf8cd746962734440
Richard Van Camp <richard.vancamp@gmail.com>
2018-03-03 13:38:42 +0200

    Feat: add youtube link in nav

--- frontend/src/components/nav/SocialLinks.js
+++ frontend/src/components/nav/SocialLinks.js
@@ -29,35 +29,20 @@ const SocialLink = styled.a`
     transform: scale(0.95);
     opacity: 0.6;
   }
-  svg {
-    path {
-      fill: currentColor;
-    }
-  }
 `
 
 const Svg = styled.svg`
-  width: ${p => toRem(Number(p.width))};
-  height: ${p => toRem(Number(p.height))};
+  width: ${p => toRem(Number(p.width || '24'))};
+  height: ${p => toRem(Number(p.height || '24'))};
   color: ${theme.iconsColor};
+  fill: ${({ fill }) => (fill ? fill : 'currentColor')};
+  stroke: ${({ stroke }) => (stroke ? stroke : 'currentColor')};
 `
 // Social icons source feather.icons.com
 // TODO: add youtube (should be added to feathers soon)
 
 const Github = () => (
-  <Svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    class="feather feather-github"
-    color="white"
-  >
+  <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
     <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
   </Svg>
 )
@@ -65,11 +50,7 @@ const Github = () => (
 const Facebook = () => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
     viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
@@ -82,11 +63,7 @@ const Facebook = () => (
 const Twitter = () => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
     viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
@@ -96,6 +73,23 @@ const Twitter = () => (
   </Svg>
 )
 
+const Youtube = () => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-youtube"
+    fill="none"
+  >
+    <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />
+    <polygon
+      fill="#5b5b5b"
+      points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"
+    />
+  </Svg>
+)
 const SocialLinks = props => (
   <Wrapper {...props}>
     <SocialLink href="https://twitter.com/webdevnsausages/">
@@ -103,6 +97,12 @@ const SocialLinks = props => (
     </SocialLink>
     <SocialLink href="https://www.facebook.com/webDevAndSausages/">
       <Facebook />
+    </SocialLink>
+    <SocialLink
+      href="https://www.youtube.com/user/Vancampit/videos"
+      fill="none"
+    >
+      <Youtube />
     </SocialLink>
     <SocialLink href="https://github.com/webDevAndSausages">
       <Github />
